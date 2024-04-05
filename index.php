2b506d0c8468f870f81a2e84ece9b34e9894ce05
jorokbro <116605805+jorokbro@users.noreply.github.com>
2024-04-06 06:06:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contoh Desain Web</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <!--sisipkan konten lain nyab-->
-</body>
-</html>
+<?php
+  include : header.php; ?>
+<?php
+  include : halaman.php; ?>
+<?php
+  include : footer.php ?>
